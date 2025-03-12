1c855dfed7eb8bf683d313d3f0bcb64ed52e4248
Ashiqur <ashiqur1801@gmail.com>
2025-03-12 15:33:39 +0600

    added

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ document.getElementById("login-btn").addEventListener("click", function () {
     const pinValuee = document.getElementById('input-pin-value').value;
 
     if (numberInput === "01877052200" && pinValuee === '13397') {
-        window.location.href = "/home.html";    
+        window.location.href = "home.html";    
     }
     else {
         alert('Incouret your input')
