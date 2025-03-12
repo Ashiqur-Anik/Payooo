1a441da419324d8684d7a3c8dca769c7e6e91ebd
Ashiqur <ashiqur1801@gmail.com>
2025-03-12 15:20:01 +0600

    added all calculatuin

--- js/home.js
+++ js/home.js
@@ -1,3 +1,4 @@
+// add money calculation
 document.getElementById('add-money-btn')
     .addEventListener('click', function () {
         const addMoneyPin = document.getElementById('add-money-pin').value;
@@ -10,7 +11,31 @@ document.getElementById('add-money-btn')
 
             const totalBalance = myBalance + addMoney;
             document.getElementById('my-balance').innerText = totalBalance;
+        }
+        else {
+            alert('Incorent your pin')
+        }
+    })
 
+// cash out calculation
+
+document.getElementById("cash-out-btn")
+    .addEventListener('click', function () {
+
+        const cashOutPin = document.getElementById('Cash-out-pin').value;
+        if (cashOutPin === "4321") {
+            const myInputBalance = document.getElementById('my-balance').innerText;
+            const cashOutInputAmount = document.getElementById('Cash-out-amount').value;
+
+            if(myInputBalance > 0 ){
+                alert('Your balance is low')
+                return
+            }
+            const myBalance = parseInt(myInputBalance);
+            const cashOutAmount = parseInt(cashOutInputAmount)
+
+            const totalBalance = myBalance - cashOutAmount;
+            document.getElementById('my-balance').innerText = totalBalance
         }
         else {
             alert('Incorent your pin')
@@ -18,4 +43,11 @@ document.getElementById('add-money-btn')
     })
 
 
-    
+document.getElementById('add-money-page-btn').addEventListener('click',function(){
+    document.getElementById('add-money-page').classList.remove('hidden');
+    document.getElementById('cash-out-page').classList.add('hidden')
+})
+document.getElementById('cash-out-page-btn').addEventListener('click',function(){
+    document.getElementById('add-money-page').classList.add('hidden');
+    document.getElementById('cash-out-page').classList.remove('hidden')
+})
--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ document.getElementById("login-btn").addEventListener("click", function () {
     const numberInput = document.getElementById('input-number-value').value;
     const pinValuee = document.getElementById('input-pin-value').value;
 
-    if (numberInput === "01877" && pinValuee === '1234') {
+    if (numberInput === "01877052200" && pinValuee === '13397') {
         window.location.href = "/home.html";    
     }
     else {
